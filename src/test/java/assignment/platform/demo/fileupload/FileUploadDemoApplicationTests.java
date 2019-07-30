9e8d7ea9f8415686472fd57d590d7464e3d0495e
walsh <walsh.zhang@hotmail.com>
2019-07-30 10:25:46 +0800

    initial version

---
+++ src/test/java/assignment/platform/demo/fileupload/FileUploadDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package assignment.platform.demo.fileupload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileUploadDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

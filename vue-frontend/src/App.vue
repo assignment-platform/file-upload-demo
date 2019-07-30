d57cd1b65b65fca703f4f7d5ba9f041fea8cdb33
walsh <walsh.zhang@hotmail.com>
2019-07-30 11:20:41 +0800

    initial version

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mt-5">
+    <div class="custom-file">
+      <input type="file" class="custom-file-input" id="customFile" />
+      <label class="custom-file-label" for="customFile">Choose file</label>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import bsCustomFileInput from 'bs-custom-file-input'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  created() {
+    bsCustomFileInput.init()
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
+

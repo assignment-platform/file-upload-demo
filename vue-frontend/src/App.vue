6abd759659a4d71ba95c6cfba3663d24f2dc5476
walsh <walsh.zhang@hotmail.com>
2019-07-30 12:40:53 +0800

    remove bootstrap

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,24 +1,15 @@
 <template>
-  <div id="app" class="container mt-5">
-    <div class="custom-file">
-      <input type="file" class="custom-file-input" id="customFile" />
-      <label class="custom-file-label" for="customFile">Choose file</label>
-    </div>
+  <div id="app">
   </div>
 </template>
 
 <script>
-import bsCustomFileInput from 'bs-custom-file-input'
 
 export default {
   name: "app",
-  created() {
-    bsCustomFileInput.init()
-  },
 };
 </script>
 
 <style scoped>
-
 </style>
 
